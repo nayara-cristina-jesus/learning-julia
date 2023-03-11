548c997eed21242c14bc4940b24aa297ee72ab33
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-03-11 00:14:46 -0300

    Update Calcula.jl

--- Calcula.jl
+++ Calcula.jl
@@ -20,7 +20,7 @@ end
 
 # Função retorna a raiz quadrada
 function f_sqrt(x)
-  sqrt(x)
+  round(sqrt(x))
 end
 
 # Função eleva um número ao quadrado
@@ -28,14 +28,38 @@ function f_x(x)
   x^2
 end
 
+# Função que calcula o seno
+function f_sin(x)
+  round(sin(x))
+end
+
+# Função que calcula o cosseno
+function f_cos(x)
+  round(cos(x))
+end
+
+# Função que calcula a tangente 
+function f_tan(x)
+  round(tan(x))
+end
+
+# Função que calcula o cotangente
+function f_cot(x)
+  round(cot(x))
+end
+
 # Entrada de dados
 x = 500
 y = 4
 
-# Chamada das Funções
+# Chamadas das Funções
 println(f_soma(x,y))
 println(f_subtrai(x,y))
 println(f_mult(x,y))
 println(f_divide(x,y))
-println(round(f_sqrt(x)))
-println(f_x(x))
+println(f_sqrt(x))
+println(f_x(x)) 
+println(f_sin(x))
+println(f_cos(x))
+println(f_tan(x))
+println(f_cot(x))
