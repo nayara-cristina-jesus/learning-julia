220c8af2d8c2a8dabe8351876881c098170ec701
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-03-11 00:03:43 -0300

    Update Calcula.jl

--- Calcula.jl
+++ Calcula.jl
@@ -19,19 +19,15 @@ function f_sqrt(x)
 end
 
 function f_x(x)
-  (x^2)
+  x^2
 end
 
 x = 500
 y = 4
 
-println("\n--------------- Calcula ---------------")
-println("\n         num1 = ",x,", num2 = ", y)
-println("\n---------------------------------------")
-println("\n1) Adição........... (", x," + ", y, ") = ", f_soma(x,y))
-println("\n2) Subtração........ (", x," - ", y, ") = ", f_subtrai(x,y))
-println("\n3) Multiplicação.... (", x," * ", y, ") = ", f_mult(x,y))
-println("\n4) Divisão.......... (", x," / ", y, ") = ", f_divide(x,y))
-println("\n5) Raiz quadrada.... ", "√", x," = ",round(f_sqrt(x)))
-println("\n6) Quadrado......... ", x,"² = ", f_x(x))
-println("\n---------------------------------------")
+println(f_soma(x,y))
+println(f_subtrai(x,y))
+println(f_mult(x,y))
+println(f_divide(x,y))
+println(round(f_sqrt(x)))
+println(f_x(x))
