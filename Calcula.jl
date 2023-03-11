17112b3339d135107efe3e6931554daef5973a7e
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-03-11 00:06:36 -0300

    Update Calcula.jl

--- Calcula.jl
+++ Calcula.jl
@@ -1,30 +1,38 @@
+# Função que soma dois números
 function f_soma(x,y)
   x + y
 end
 
+# Função que subtrai dois números
 function f_subtrai(x,y)
   x - y
 end
 
+# Função que multiplica dois números
 function f_mult(x,y)
   x * y
 end
 
+# Função que divide dois números
 function f_divide(x,y)
   x / y
 end
 
+# Função retorna a raiz quadrada
 function f_sqrt(x)
   sqrt(x)
 end
 
+# Função eleva um número ao quadrado
 function f_x(x)
   x^2
 end
 
+# Entrada de dados
 x = 500
 y = 4
 
+# Chamadas das Funções
 println(f_soma(x,y))
 println(f_subtrai(x,y))
 println(f_mult(x,y))
